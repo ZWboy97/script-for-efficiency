5ed979d7c933b85c5942678aa0b00ef70ce3b989
JiaChang.Li <1769264507@qq.com>
2020-10-11 10:44:19 +0800

    ADD: ding_talk script

---
+++ Python/ding_talk/sendmsg.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+# 这个 url 从 PC 端钉钉群组->管理机器人里获得
+dingding_url = "https://oapi.dingtalk.com/robot/send?access_token=3884f496622bb7ca86bae635726729e4fbef4d5bxxxxxxxxxxxxxxxx"
+
+headers = {"Content-Type": "application/json; charset=utf-8"}
+
+post_data = {
+    "msgtype": "text",
+    "text": {
+        "content": u"运维消息：服务异常"
+    },
+    "at": {
+        "atMobiles": ["188*****021"]
+    }
+}
+
+r = requests.post(dingding_url, headers=headers,
+                  data=json.dumps(post_data))
+print(r.content)
